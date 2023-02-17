5322422f265d72eb635c2de3d2b427d06cf4eb94
Berenguer Pou <berenguer.pou@gmail.com>
2023-02-17 13:39:45 +0100

    add meta:og tags / app basic layout

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LittleLemonLogo from "../assets/img/Logo.svg";
+
+function Header() {
+  return (
+    <header>
+      <img src={LittleLemonLogo} alt="Little Lemon Restaurant logo" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#d">a</a>
+        </li>
+        <li>
+          <a href="#d">b</a>
+        </li>
+        <li>
+          <a href="#d">c</a>
+        </li>
+        <li>
+          <a href="#d">d</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
