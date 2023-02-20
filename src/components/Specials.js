53d00fb8b062f390cfb7698a0513dc0c02f9a8d0
Berenguer Pou <berenguer.pou@gmail.com>
2023-02-20 20:16:56 +0100

    add specials section and mock articles

---
+++ src/components/Specials.js
@@ -0,0 +1,62 @@
+import React from "react";
+import BruschettaImg from "../assets/img/bruschetta.jpg";
+
+function Specials() {
+  return (
+    <section id="specials">
+      <div className="specials-top">
+        <h2>Specials</h2>
+        <button>Online Menu</button>
+      </div>
+      <div className="specials-list">
+        <article className="dish-card">
+          <img className="special-img" src={BruschettaImg} alt="bruschetta" />
+          <div className="special-title-price">
+            <h3 className="special-title">Bruschetta</h3>
+            <h3 className="special-price">$12.99</h3>
+          </div>
+          <p className="special-description">
+            Our Bruschetta is made from grilled bread that has been smeared with
+            garlic and seasoned with salt and olive oil.
+          </p>
+          <div className="special-delivery">
+            <p className="special-order">Order a delivery</p>
+            <icon>moto icon</icon>
+          </div>
+        </article>
+        <article className="dish-card">
+          <img className="special-img" src={BruschettaImg} alt="bruschetta" />
+          <div className="special-title-price">
+            <h3 className="special-title">Bruschetta</h3>
+            <h3 className="special-price">$12.99</h3>
+          </div>
+          <p className="special-description">
+            Our Bruschetta is made from grilled bread that has been smeared with
+            garlic and seasoned with salt and olive oil.
+          </p>
+          <div className="special-delivery">
+            <p className="special-order">Order a delivery</p>
+            <icon>moto icon</icon>
+          </div>
+        </article>
+        <article className="dish-card">
+          <img className="special-img" src={BruschettaImg} alt="bruschetta" />
+          <div className="special-title-price">
+            <h3 className="special-title">Bruschetta</h3>
+            <h3 className="special-price">$12.99</h3>
+          </div>
+          <p className="special-description">
+            Our Bruschetta is made from grilled bread that has been smeared with
+            garlic and seasoned with salt and olive oil.
+          </p>
+          <div className="special-delivery">
+            <p className="special-order">Order a delivery</p>
+            <icon>moto icon</icon>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+}
+
+export default Specials;
